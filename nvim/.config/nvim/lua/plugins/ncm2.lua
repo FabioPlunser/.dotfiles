b96c05159dd541e837b7e9f230a0d8ab46f355d6
Fabio Plunser <fabio.plunser@student.uibk.ac.at>
2024-04-23 21:38:04 +0200

    added nvim plugins and updated tmux-sessionizer

---
+++ nvim/.config/nvim/lua/plugins/ncm2.lua
@@ -0,0 +1,18 @@
+return {
+  { "ncm2/ncm2" },
+  {
+    "roxma/nvim-yarp",
+  },
+  {
+    "ncm2/ncm2-bufword",
+  },
+  {
+    "ncm2/ncm2-path",
+  },
+  {
+    "roxma/nvim-yarp",
+  },
+  {
+    "phpactor/ncm2-phpactor",
+  },
+}
